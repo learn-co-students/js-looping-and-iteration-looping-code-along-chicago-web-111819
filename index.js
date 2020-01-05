f01c9e592a6983c0c97bbd8c049c12737da8a696
dut-a <d.dut@live.com>
2020-01-05 13:26:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        let msg = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(msg);
+    }
+    return messages;
+}
+
+function countDown(startingNumber) {
+    let i = startingNumber;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+countDown(13);
+
+
+
